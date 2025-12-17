1887e29bdb064503bb0a6928c0c78d2423eaade2
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-12-17 19:38:32 +0500

    Update FastAPI prediction endpoint for v2

--- src/main.py
+++ src/main.py
@@ -282,3 +282,5 @@ if __name__ == '__main__':
 # Update FastAPI prediction endpoint for v2 [2025-12-15T20:14:25]
 
 # Add monitoring alert webhooks for Slack [2025-12-17T18:50:22]
+
+# Update FastAPI prediction endpoint for v2 [2025-12-17T19:38:32]
