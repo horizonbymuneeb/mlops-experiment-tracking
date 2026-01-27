2173ff1be2fe18cf6d649ae62ccf5a5fcc3564af
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-01-27 17:45:14 +0500

    Add monitoring alert webhooks for Slack

--- src/utils.py
+++ src/utils.py
@@ -163,3 +163,5 @@ def format_number(n: int) -> str:
 # Implement Prometheus metrics collector service [2026-01-27T10:31:56]
 
 # Fix race condition in metrics aggregation [2026-01-27T11:04:32]
+
+# Add monitoring alert webhooks for Slack [2026-01-27T17:45:14]
